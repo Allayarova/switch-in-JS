f8188ab9340b3d42fd928e25fc3b12df3d12c0cc
Allayarova Mahliyo <117286548+Allayarova@users.noreply.github.com>
2023-02-11 21:11:51 +0300

    Add files via upload

---
+++ switch in js/index.js
@@ -0,0 +1,19 @@
+function bos()
+{
+
+
+let t=document.f1.til;
+
+if ( t.value=="uz"){
+    document.f1.language.value="u";
+}
+else if (t.value=="ru"){
+    document.f1.language.value="r";
+
+}
+else if (t.value=="en")
+{
+    document.f1.language.value="e";
+
+}
+}
